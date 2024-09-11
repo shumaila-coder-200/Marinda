b0236ed62ca6856a248c1f686e9f329a0f04f501
*shumaila-coder-200* <shumailaraza766@gmail.com>
2024-09-11 13:41:03 -0600

    First Commit

---
+++ Miranda.js
@@ -0,0 +1,23 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+let tl = gsap.timeline();
+tl.to("#page", {
+    y: "100vh",
+    duration: 0,
+    scale:0.6,
+})
+tl.to("#page", {
+    y: "30vh",
+    duration: 1,
+    delay: 1,
+})
+tl.to("#page", {
+    y: "0vh",
+    rotate:360,
+    duration: 0.8,
+    scale:1,
+})
+
+let image=document.querySelector("#page6")
